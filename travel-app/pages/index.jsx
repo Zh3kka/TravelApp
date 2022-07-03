21206a630591dd8cb651f4c1a339b149a39ee9d4
Zheka <Zh3kaSel@yandex.ru>
2022-07-03 21:12:39 +0300

    Create footer

---
+++ travel-app/pages/index.jsx
@@ -0,0 +1,10 @@
+import Footer from "../app/components/common/Footer/Footer";
+import styles from "../assets/styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Footer />
+    </div>
+  );
+}
